d3f9ce50d504b9d91016e3d0ea02c78668b0f702
Toothless <hyunjinney@naver.com>
2025-02-04 09:15:01 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace L20250204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[53];
+            bool[] isSelected = new bool[53];
+            int cnt = 0;
+            for (int i = 1; i <= 52; i++)
+            {
+                arr[i] = i;
+            }
+            while (cnt<8)
+            {
+                Random randomObj = new Random();
+                int randomNum = randomObj.Next(1, 52);
+                if (isSelected[randomNum])
+                {
+                    continue;
+                }
+                isSelected[randomNum] = true;
+                cnt++;
+            }
+            for (int i = 0; i < 52; i++)
+            {
+                if (isSelected[i])
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
